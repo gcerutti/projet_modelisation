412f29c3337d1bf17ccdf25af88a0ca7a8063c08
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2018-09-18 13:38:31 +0200

    une superbe classe de nucleosome

---
+++ nucleosome.py
@@ -0,0 +1,10 @@
+class Nucleosome(object):
+
+    def __init__(self, n_histones=2):
+        self.n_histones = n_histones
+        self.histones = None
+
+        self.initialize()
+
+    def initialize(self):
+        self.histones = [[0,0] for h in range(self.n_histones)]
