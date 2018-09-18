8524d6fbf579e7c79c43f25b8b9d4cd2fbcc90bf
Guillaume Cerutti <guillaume.cerutti@inria.fr>
2018-09-18 13:49:35 +0200

    added histone class

--- nucleosome.py
+++ nucleosome.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+class Histone(object):
+
+    def __init__(self, state, nucleation):
+        self.state = state
+        self.nucleation = nucleation
+
+    def __str__(self):
+        return "Histone(S"+str(self.state)+("+N)" if self.nucleation else ")")
+
 class Nucleosome(object):
 
     def __init__(self, n_histones=2):
@@ -8,5 +17,10 @@ class Nucleosome(object):
 
         self.initialize()
 
+    def __str__(self):
+        return "".join([str(h)+" "  for h in self.histones])     
+
+
     def initialize(self):
-        self.histones = np.array([[0,0] for h in range(self.n_histones)])
+        self.histones = [Histone(0,0) for h in range(self.n_histones)]
+
